4adeb577057ebb800365e461e5e467b06f683e13
vamsi3755 <sirigirivamsikrishna2000@gmail.com>
2026-01-26 23:10:02 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -4,7 +4,6 @@
 
 
 #! /bin/bash
-aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
@@ -13,6 +12,6 @@ mv kops-linux-amd64 /usr/local/bin/kops
 
 aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+export KOPS_STATE_STORE=s3://vamsi375.k8s.local
+kops create cluster --name vamsi.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --master-volume-size 30 --node-count=2 --node-size t2.micro --node-volume-size 28
 kops update cluster --name rahams.k8s.local --yes --admin
