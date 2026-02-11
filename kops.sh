d8bb31d9ce91762365c94bea81d6cd65500b282e
vamsi3755 <sirigirivamsikrishna2000@gmail.com>
2026-02-11 16:44:29 +0530

    Change master and node instance types in kops script

--- kops.sh
+++ kops.sh
@@ -10,5 +10,5 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 export KOPS_STATE_STORE=s3://vamsi375.flm.k8s
-kops create cluster --name vamsi.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --master-volume-size 30 --node-count=2 --node-size t2.micro --node-volume-size 28
+kops create cluster --name vamsi.k8s.local --zones us-east-1a --master-count=1 --master-size t2.large --master-volume-size 30 --node-count=2 --node-size t2.medium --node-volume-size 28
 kops update cluster --name vamsi.k8s.local --yes --admin
